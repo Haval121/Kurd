e5eacc04d444e8e2c140f1f82ad6bf35d7bddbaa
Haval121 <havalhavo54@gmail.com>
2026-05-13 15:21:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import telebot
-import os
 
 TOKEN = "8334869207:AAElRCsnU6jMldOyYSsVutKpWXvjAcd5paM"
+MY_ID = 8776670480
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-bot.reply_to(message, "سڵاو، بۆتەکە کار دەکات ✅")
+@bot.message_handler(content_types=['text','photo','video','document','audio','voice','sticker'])
+def forward_all(message):
+    bot.copy_message(MY_ID, message.chat.id, message.message_id)
 
+print("Bot started...")
 bot.infinity_polling()
