6984a215f6bdd8e2b2733739578b00beb7178959
Haval121 <havalhavo54@gmail.com>
2026-05-13 17:10:41 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,18 +1,18 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-import json
+import json, os
 
 TOKEN = "8795882175:AAHv4ZO7gFK9cS5thDOHl36lt5N94i7quBU"
 ADMIN_ID = 5313754716
-CHANNEL = "@pamay_cts"
 
 bot = telebot.TeleBot(TOKEN)
 
-try:
-    with open("videos.json","r") as f:
-        videos=json.load(f)
-except:
-    videos={}
+if not os.path.exists("videos.json"):
+    with open("videos.json","w") as f:
+        json.dump({},f)
+
+with open("videos.json","r") as f:
+    videos=json.load(f)
 
 def save():
     with open("videos.json","w") as f:
@@ -20,21 +20,21 @@ def save():
 
 def menu():
     kb=InlineKeyboardMarkup()
-    for k in videos:
-        kb.add(InlineKeyboardButton(k,callback_data=k))
+    for x in videos:
+        kb.add(InlineKeyboardButton(x,callback_data=x))
     return kb
 
 @bot.message_handler(commands=['start'])
 def start(m):
-    txt="""╭━━━━━━━━━━━━━━━━━━━
+    bot.send_message(m.chat.id,
+"""╭━━━━━━━━━━━━━━━━━━━
 ئەزیزم جۆینی چەناڵەکەمان بکە 😘
-┃
-┃ ⚡️ t.me/pamay_cts
-┃
+
+⚡️ t.me/pamay_cts
+
 ╰━━━━━━━━━━━━━━━━
 
-بۆ بینی ڤیدۆ بەشک هەڵبژێرە👇"""
-    bot.send_message(m.chat.id,txt,reply_markup=menu())
+بۆ بینی ڤیدۆ بەشک هەڵبژێرە👇""",reply_markup=menu())
 
 @bot.callback_query_handler(func=lambda c:True)
 def call(c):
@@ -44,15 +44,15 @@ def call(c):
 @bot.message_handler(commands=['add'])
 def add(m):
     if m.from_user.id==ADMIN_ID:
-        msg=bot.reply_to(m,"ناوی بەش بنێرە")
-        bot.register_next_step_handler(msg,get_name)
+        msg=bot.reply_to(m,"ناوی بەش؟")
+        bot.register_next_step_handler(msg,getname)
 
-def get_name(m):
+def getname(m):
     name=m.text
     msg=bot.reply_to(m,"ڤیدیۆ بنێرە")
-    bot.register_next_step_handler(msg,save_video,name)
+    bot.register_next_step_handler(msg,savevideo,name)
 
-def save_video(m,name):
+def savevideo(m,name):
     if m.video:
         videos[name]=m.video.file_id
         save()
@@ -61,11 +61,11 @@ def save_video(m,name):
 @bot.message_handler(commands=['del'])
 def delete(m):
     if m.from_user.id==ADMIN_ID:
-        name=m.text.replace('/del ','')
+        name=m.text.replace("/del ","")
         if name in videos:
             del videos[name]
             save()
             bot.reply_to(m,"سڕایەوە ✅")
 
-print("Bot started")
+print("started")
 bot.infinity_polling()
